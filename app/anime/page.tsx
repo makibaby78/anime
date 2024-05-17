889b3c12c78f7736dbc0504f53fd685a7e95018f
Max Loued Baisac <82240891+makibaby78@users.noreply.github.com>
2024-05-17 12:59:16 +0800

    Update page.tsx

--- app/anime/page.tsx
+++ app/anime/page.tsx
@@ -7,6 +7,6 @@ export default async function Anime() {
   const top = (await res.json()).data;
 
   return (
-    // <Listing details={top} title="Top Rated Anime" for="anime" />
+    <Listing details={top} title="Top Rated Anime" for="anime" />
   );
 }
